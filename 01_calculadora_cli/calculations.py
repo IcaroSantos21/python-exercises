7fafd559c7bb1910e2e38fe015407c1d0281ad77
Icaro Rodrigues <icarord13@gmail.com>
2025-12-27 17:06:40 -0300

    Implementa validação de opção e controle de fluxo principal

---
+++ 01_calculadora_cli/calculations.py
@@ -0,0 +1,25 @@
+def sum_(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def division(num1, num2):
+    result = num1 / num2
+    return result
+
+def enhance(num1, num2):
+    result = num1 ** num2
+    return result
+
+def percentage(num1, num2):
+    result = num1 * (num2 / 100)
+    return result
+
+operators = {'+', '-', 'x', '/', '*', '%',}
