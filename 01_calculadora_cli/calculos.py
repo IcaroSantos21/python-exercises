168cf046370d3da6b3b30ee87c5c46c2c1c6981f
Icaro Rodrigues <icarord13@gmail.com>
2025-12-26 22:04:06 -0300

    criação do arquivo e das funções de calcúlos

---
+++ 01_calculadora_cli/calculos.py
@@ -0,0 +1,23 @@
+def soma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+def subtracao(num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+def multiplicacao(num1, num2):
+    resultado = num1 * num2
+    return resultado
+
+def divisao(num1, num2):
+    resultado = num1 / num2
+    return resultado
+
+def potencializacao(num1, num2):
+    resultado = num1 ** num2
+    return resultado
+
+def porcentagem(num1, num2):
+    resultado = num1 * (num2 / 100)
+    return resultado
