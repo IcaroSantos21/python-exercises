7db637ca0a1affae7e10748e1b7853d9d87ee47a
Icaro Rodrigues <icarord13@gmail.com>
2025-12-30 01:43:09 -0300

    Fix CPF validation edge cases

--- 02_validador_cpf/cpf_utils.py
+++ 02_validador_cpf/cpf_utils.py
@@ -10,7 +10,8 @@ def first_digit_validation(digits):
         num = 10 - i
         total += list_int[i] * num
     result = (total * 10) % 11
-
+    if result == 10:
+        return 0
     return result
 
 def second_digit_validation(digits):
@@ -21,5 +22,23 @@ def second_digit_validation(digits):
         num = 11 - i
         total += list_int[i] * num
     result = (total * 10) % 11
+    if result == 10:
+        return 0
 
     return result
+
+def validate_cpf(cpf: str) -> bool:
+    cpf_str = normalization_str(cpf)  
+    if len(cpf_str) != 11:
+        return False
+    if len(set(cpf_str)) == 1:
+        return False
+    
+    first_digit = str(first_digit_validation(cpf_str))
+    if first_digit != cpf_str[9]:
+        return False
+    
+    second_digit = str(second_digit_validation(cpf_str))
+    if second_digit != cpf_str[10]:
+        return False
+    return True
--- 02_validador_cpf/main.py
+++ 02_validador_cpf/main.py
@@ -2,8 +2,8 @@ from cpf_utils import *
 
 while True:
     cpf = str(input('Entre com o cpf: '))
-    
-    cpf_digits = normalization_str(cpf)
-    first_digit_validation(cpf_digits)
-    second_digit_validation(cpf_digits)
+    if validate_cpf(cpf) == False:
+        print('CPF Inválido')
+        break
+    print('CPF Válido')
     break
