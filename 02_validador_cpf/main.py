14577d1f4d1d64111c7566f3c5a1131364422355
Icaro Rodrigues <icarord13@gmail.com>
2025-12-30 01:52:47 -0300

    Implement menu for CLI

--- 02_validador_cpf/main.py
+++ 02_validador_cpf/main.py
@@ -1,9 +1,17 @@
 from cpf_utils import *
+from menu import *
 
 while True:
-    cpf = str(input('Entre com o cpf: '))
-    if validate_cpf(cpf) == False:
-        print('CPF Inválido')
+    display_menu()
+    option = input('Digite aqui:')
+    valid_option = parse_option(option)
+    if valid_option == 'v':
+        cpf = str(input('Entre com o cpf: '))
+        if validate_cpf(cpf) == False:
+            print('CPF Inválido')
+        print('CPF Válido')
+    elif valid_option == 's':
+        print('saindo...')
         break
-    print('CPF Válido')
-    break
+    else:
+        print('Opção inválida')
---
+++ 02_validador_cpf/menu.py
@@ -0,0 +1,8 @@
+def display_menu():
+    print("----------VALIDADOR DE CPF----------")
+    print("             ESCOLHA:")
+    print("             Validar = (v)")
+    print("             Sair = (s)")
+
+def parse_option(option):   
+    return option.lower().strip()
